33fe2a76dbcf9268bcf6eb29c28fbdaf40e1a863
Alberto Chio <a.chio@outlook.com>
2020-12-29 00:00:10 -0600

    asdsssd

--- src/app/survey/aplicacion/new-answer/new-answer.component.ts
+++ src/app/survey/aplicacion/new-answer/new-answer.component.ts
@@ -53,7 +53,7 @@ export class NewAnswerComponent implements OnInit {
         this.toastr.error(err.error.mensaje, 'Fail', {
           timeOut: 3000,  positionClass: 'toast-top-center',
         });
-         //this.router.navigate(['/']);
+         this.router.navigate(['survey/lista']);
       }
     );
 
